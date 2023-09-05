aab3ce4d501e67c75e18306000b5e8978fac3cfc
Awuku Baah Charles <charlesawukubaah277@gmail.com>
2023-09-05 16:46:39 +0100

    leds

--- test.py
+++ test.py
@@ -13,8 +13,13 @@ if __name__ == "__main__":
 """
 
 while True:
+    # LED(22).on()
+    # LED(27).on()
     Buzzer(23).on()
     print('LED on')
+    sleep(2)
+    Buzzer(23).off()
+    sleep(2)
     # sleep(2)
     # Buzzer(27).off()
     # print('LED off')

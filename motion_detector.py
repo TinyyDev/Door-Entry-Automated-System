154bbc988b578ae7405bb6e5f73c330bbc8c9a1d
Awuku Baah Charles <charlesawukubaah277@gmail.com>
2023-09-06 13:30:54 +0100

    leds

--- motion_detector.py
+++ motion_detector.py
@@ -2,7 +2,7 @@ from time import sleep
 from gpiozero import LED, Buzzer, MotionSensor
 
 
-def buzzer(PIN=23):
+def buzzer_on(PIN=23):
     buzz = Buzzer(PIN)
     buzz.on()
     sleep(0.9)
@@ -10,35 +10,43 @@ def buzzer(PIN=23):
     sleep(0.1)
     
 def red_light_on(PIN=22):
-    return LED(PIN).on()
+    red = LED(PIN)
+    red.on()
 
 def red_light_off(PIN=22):
-    return LED(PIN).on()
+    red = LED(PIN)
+    red.off()
     
 def green_light_on(PIN=27):
-    return LED(PIN).on()
+    green = LED(PIN)
+    green.on()
 
-def green_light_buzzer_off(PIN=27):
-    LED(PIN).off()
-    Buzzer(PIN).off()
+def green_light_off(PIN=27):
+    green = LED(PIN)
+    green.on()
+    
+def buzzer_off(PIN=23):
+    buzzer = Buzzer(PIN)
+    buzzer.off()
     
 def main():
     sensor = MotionSensor(17)
-    LED(22).on() # red led
+    red_light_on()
     try:
         while True:
             if sensor.value == 1:
-                LED(22).off() # red led
+                red_light_off() # red led
+                buzzer_on()
                 print('Motion detected')
-                buzzer()
-                LED(27).on() # green led
-                sleep(2)
+                sleep(1.5)
             elif sensor.value == 0:
-                LED(27).on() # green led
+                green_light_off()
+                buzzer_off()
+                red_light_on()
+                red_light_on()
                 print('Motion not detected')
                 print('Sleep mode :)')
-                red_light_on()
-                sleep(2)
+                sleep(1.5)
     except KeyboardInterrupt:
         print('Exiting with ctrl-c!!!')
         
--- test.py
+++ test.py
@@ -1,26 +1,7 @@
 from time import sleep
-import motion_detector
 from gpiozero import LED, Buzzer, Button
 
-"""
-if __name__ == "__main__":
-    while True:
-        if motion_detector.motion_detect():
-            print("Somebody is closing")
-        else:
-            print("Nobody")
-        time.sleep(1)
-"""
-
 while True:
-    # LED(22).on()
-    # LED(27).on()
-    Buzzer(23).on()
-    print('LED on')
-    sleep(2)
-    Buzzer(23).off()
-    sleep(2)
-    # sleep(2)
-    # Buzzer(27).off()
-    # print('LED off')
-    # sleep(2)
+    LED(22).on()
+    LED(27).on()
+    

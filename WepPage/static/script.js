dcbaf78070d7fed32a985232013a4f3e8624fc4f
Awuku Baah Charles <charlesawukubaah277@gmail.com>
2023-09-06 15:19:46 +0100

    added main app

---
+++ WepPage/static/script.js
@@ -0,0 +1,21 @@
+// static/script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const motionStatusElement = document.getElementById("motion-status");
+
+    // Function to update motion status
+    function updateMotionStatus() {
+        // Replace this with your code to fetch motion sensor status
+        // You might use AJAX or other methods here
+        const motionStatus = "Motion Detected";  // Sample status, replace with actual data
+        motionStatusElement.innerText = motionStatus;
+    }
+
+    // Initial status update
+    updateMotionStatus();
+
+    // Function to refresh the status at regular intervals (e.g., every 5 seconds)
+    setInterval(function() {
+        updateMotionStatus();
+    }, 2000); // 5000 milliseconds = 5 seconds
+});

fc68ffb706e55284dece124930af3f3358aefeac
Awuku Baah Charles <charlesawukubaah277@gmail.com>
2023-09-06 14:58:40 +0100

    started a wepapp

--- motion_detector.py
+++ motion_detector.py
@@ -9,41 +9,27 @@ def buzzer_on(PIN=23):
     buzz.off()
     sleep(0.1)
     
-def red_light_on(PIN=22):
-    red = LED(PIN)
-    red.on()
-
-def red_light_off(PIN=22):
-    red = LED(PIN)
-    red.off()
-    
-def green_light_on(PIN=27):
-    green = LED(PIN)
-    green.on()
-
-def green_light_off(PIN=27):
-    green = LED(PIN)
-    green.on()
-    
 def buzzer_off(PIN=23):
-    buzzer = Buzzer(PIN)
-    buzzer.off()
+    buzz = Buzzer(PIN)
+    buzz.off()
     
 def main():
     sensor = MotionSensor(17)
-    red_light_on()
+    red = LED(22)
+    green = LED(27)
+    
     try:
         while True:
             if sensor.value == 1:
-                red_light_off() # red led
+                red.off()
+                green.on()
                 buzzer_on()
                 print('Motion detected')
-                sleep(1.5)
+                sleep(2)
             elif sensor.value == 0:
-                green_light_off()
+                green.off()
                 buzzer_off()
-                red_light_on()
-                red_light_on()
+                red.on()
                 print('Motion not detected')
                 print('Sleep mode :)')
                 sleep(1.5)

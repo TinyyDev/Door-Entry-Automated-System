7740e178a6c9b8e9bd27af1ce96b37d062c7acd6
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-09-08 07:58:31 +0000

    created new webapp

--- WebPage/app.py
+++ WebPage/app.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('./public/tinnydev.html')
 
-
+@app.route('/main')
+def main():
+    return render_template('./public/main.html')
 
 
 if __name__ == '__main__':
